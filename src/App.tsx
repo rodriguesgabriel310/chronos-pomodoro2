7c4c9a2eace7e3608e1b573385229279768c7b51
Gabriel Rodrigues <rodriguesdesousa310@gmail.com>
2025-07-26 16:43:51 -0300

    alterações 26-07

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './styles/theme.css';
+import './styles/global.css';
 
-  return (
+
+
+export function App(){
+    console.log('olá');
+
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading attr={123} attr2='String'>Olá</Heading>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+        elit. Deleniti, unde repudiandae. Molestias vel optio
+         quis, dolore voluptatem velit. Repellendus quod tenetur
+          obcaecati ut inventore consequuntur 
+        excepturi cumque earum minima placeat?
       </p>
     </>
-  )
+    );
 }
 
-export default App
+
