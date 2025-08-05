40093e0b41c79c3e1d0551363eea13be6344a307
Gabriel Rodrigues <rodriguesdesousa310@gmail.com>
2025-08-05 17:59:22 -0300

    menu e logo prontos

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
+
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 
 export function App(){
     return (
     <>
-      <Heading>
-        History
-        <button>
-          <TimerIcon />
-         </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-        elit. Deleniti, unde repudiandae. Molestias vel optio
-         quis, dolore voluptatem velit. Repellendus quod tenetur
-          obcaecati ut inventore consequuntur 
-        excepturi cumque earum minima placeat?
-      </p>
+      <Container>
+       <Logo />
+      </Container>  
+
+      <Container>
+        <Menu />
+      </Container> 
+
+       <Container>
+        <CountDown />
+      </Container> 
+
+      
     </>
-    );
+   ); 
 }
 
 
