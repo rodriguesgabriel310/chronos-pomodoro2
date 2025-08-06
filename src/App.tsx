9c05195a035c3d2c241252ea7f0a1082ad72f46d
Gabriel Rodrigues <rodriguesdesousa310@gmail.com>
2025-08-05 22:54:26 -0300

    formulário feito

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 
 export function App(){
@@ -23,7 +27,30 @@ export function App(){
         <CountDown />
       </Container> 
 
-      
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='task' 
+            id='meuInput' 
+            type='text'
+            placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+           <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+            
+          </div>
+        </form>
+      </Container> 
     </>
    ); 
 }
